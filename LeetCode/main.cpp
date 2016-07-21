e73ba5f32182e316b0c3cfe05b6d8b1adb84512f
Jack--Ma <100858433@qq.com>
2016-07-21 15:00:39 +0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by JackMa on 16/7/21.
+//  Copyright © 2016年 JackMa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
