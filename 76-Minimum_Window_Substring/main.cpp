f083bdc026857dfb50f03c267b8d7c0bec7b9de4
Jack--Ma <100858433@qq.com>
2016-08-10 14:47:05 +0800

    Fix 76

--- 76-Minimum_Window_Substring/main.cpp
+++ 76-Minimum_Window_Substring/main.cpp
@@ -13,87 +13,6 @@
 
 using namespace std;
 
-//void findMinWindow(unordered_map<char, vector<int>> &map, unordered_map<char, vector<int>>::iterator iter, int last_num, int min_num, int max_num, int &final_min, int &final_max) {
-//    
-//    if (iter == map.end()) {
-//        if ((max_num-min_num) < (final_max - final_min)) {
-//            final_min = min_num;
-//            final_max = max_num;
-//        }
-//        
-//        return;
-//    }
-//    char this_char = iter->first;
-//    vector<int> this_nums = map[this_char];
-//    
-//    //找到正确的位置
-//    int position = 0;
-//    while (position < this_nums.size()) {
-//        if (this_nums[position] > last_num) {
-//            break;
-//        }
-//        position++;
-//    }
-//    //分别判断
-//    if (position > 0 && position < this_nums.size()) {
-//        int this_num = this_nums[position-1];
-//        int this_min_num = min(min_num, this_num);
-//        int this_max_num = max(max_num, this_num);
-//        auto temp_iter = iter;
-//        findMinWindow(map, ++temp_iter, this_num, this_min_num, this_max_num, final_min, final_max);
-//        
-//        this_num = this_nums[position];
-//        this_min_num = min(min_num, this_num);
-//        this_max_num = max(max_num, this_num);
-//        temp_iter = iter;
-//        findMinWindow(map, ++temp_iter, this_num, this_min_num, this_max_num, final_min, final_max);
-//    } else if (position == this_nums.size()) {
-//        int this_num = this_nums[position-1];
-//        int this_min_num = min(min_num, this_num);
-//        int this_max_num = max(max_num, this_num);
-//        findMinWindow(map, ++iter, this_num, this_min_num, this_max_num, final_min, final_max);
-//    } else if (position == 0) {
-//        int this_num = this_nums[position];
-//        int this_min_num = min(min_num, this_num);
-//        int this_max_num = max(max_num, this_num);
-//        findMinWindow(map, ++iter, this_num, this_min_num, this_max_num, final_min, final_max);
-//    }
-//    
-//    return;
-//}
-//
-//string minWindow(string s, string t) {
-//    string result = "";
-//    unordered_map<char, vector<int>> map;
-//    
-//    for (int i = 0; i < s.size(); i++) {
-//        char this_char = s[i];
-//        if (t.find(this_char) != string::npos) {
-//            map[this_char].push_back(i);
-//        }
-//    }
-//    
-//    if (map.size() == 0) {
-//        return "";
-//    }
-//    if (map.size() == 1) {
-//        vector<int> nums = map.begin()->second;
-//        result = result.assign(s, nums[0], t.size());
-//        return result;
-//    }
-//    
-//    int final_min = 0, final_max = int(s.size());
-//    char first_char = map.begin()->first;
-//    unordered_map<char, vector<int>>::iterator second_iter = ++map.begin();
-//    
-//    for (int i = 0; i < map[first_char].size(); i++) {
-//        findMinWindow(map, second_iter, map[first_char][i], map[first_char][i], map[first_char][i], final_min, final_max);
-//    }
-//    
-//    result = result.assign(s, final_min, final_max-final_min+1);
-//    return result;
-//}
-
 string minWindow(string S, string T) {
     if (S.empty()) {
         return "";
@@ -143,6 +62,7 @@ string minWindow(string S, string T) {
     }
     
     if (minWidth == INT_MAX)  {
+        //T中有S中不存在的字符
         return "";
     } else {
         return S.substr(min_start, minWidth);
@@ -151,7 +71,7 @@ string minWindow(string S, string T) {
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    string s = "ADOBECODEBANC", t = "ABC";
+    string s = "ADOBECODEBANC", t = "ABCY";
     cout << minWindow(s, t) << endl;
     
     return 0;
