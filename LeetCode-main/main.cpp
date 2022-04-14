7072e8b4b22a6f5f9e485e11887df7a6afcecf7f
JackMa <100858433@qq.com>
2022-04-14 09:08:03 +0800

    Optimize: Add recursion method
    21. Merge Two Sorted Lists

--- LeetCode-main/main.cpp
+++ LeetCode-main/main.cpp
@@ -7,13 +7,13 @@
 //
 
 #include <iostream>
-#include "RottingOranges.hpp"
+#include "MergeTwoSortedLists.hpp"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     
-    testRottingOranges();
+    testMergeTwoLists();
     
     return 0;
 }
--- Linked List/21. Merge Two Sorted Lists /MergeTwoSortedLists.cpp
+++ Linked List/21. Merge Two Sorted Lists /MergeTwoSortedLists.cpp
@@ -13,9 +13,31 @@
  Output: [1,1,2,3,4,4]
  */
 
+void _mergeTwoLists_recursion(ListNode *list1, ListNode *list2, ListNode *next) {
+    if (!list1) {
+        next->next = list2;
+        return;
+    }
+    if (!list2) {
+        next->next = list1;
+        return;
+    }
+    if (list1->val > list2->val) {
+        next->next = list2;
+        _mergeTwoLists_recursion(list1, list2->next, next->next);
+    } else {
+        next->next = list1;
+        _mergeTwoLists_recursion(list1->next, list2, next->next);
+    }
+}
+
 // my solution
 ListNode* Solution::mergeTwoLists(ListNode *list1, ListNode *list2) {
     ListNode *result = new ListNode(0);
+    
+    ListNode *k = result;
+    _mergeTwoLists_recursion(list1, list2, k);
+    return result->next;
     
     ListNode *p = list1, *q = list2;
     ListNode *head = result;
