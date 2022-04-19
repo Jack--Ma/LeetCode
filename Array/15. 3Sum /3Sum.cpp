c4e60444929410f57ef3930bae947f490d1136fd
mazhaoxiang <mazhaoxiang@bytedance.com>
2022-04-19 19:41:15 +0800

    Optimize: Add two pointers method
    15. 3Sum

--- Array/15. 3Sum /3Sum.cpp
+++ Array/15. 3Sum /3Sum.cpp
@@ -52,6 +52,44 @@ vector<vector<int>> _threeSum_timeout(vector<int>& nums) {
     return result;
 }
 
+// solution by use two pointers
+vector<vector<int>> _threeSum_two_pointers(vector<int>& nums) {
+    const int size = (int)nums.size();
+    if (size < 3) {
+        return {};
+    }
+    vector<vector<int>> result;
+    sort(nums.begin(), nums.end());
+    
+    for (int i = 0; i < size - 2; i++) {
+        int num_i = nums[i];
+        if (num_i > 0) {
+            continue;
+        }
+        if (i > 0 && num_i == nums[i-1]) {
+            continue;
+        }
+        
+        int j = i + 1, k = size - 1;
+        while (j < k) {
+            int num_j = nums[j], num_k = nums[k];
+            if (j > i + 1 && num_j == nums[j-1]) {
+                j++;
+                continue;
+            }
+            if (num_i + num_j + num_k < 0) {
+                j++;
+            } else if (num_i + num_j + num_k > 0) {
+                k--;
+            } else if (num_i + num_j + num_k == 0) {
+                result.push_back({num_i, num_j, num_k});
+                j++;
+            }
+        }
+    }
+    return result;
+}
+
 // better solution
 vector<vector<int>> Solution::threeSum(vector<int>& nums) {
     if (nums.size() < 3) {
@@ -93,6 +131,6 @@ vector<vector<int>> Solution::threeSum(vector<int>& nums) {
 }
 
 void testThreeSum() {
-    vector<int> nums = {0,0,0,0};
-    Solution().threeSum(nums);
+    vector<int> nums = {-1,0,1,2,-1,-4};
+    printVector(Solution().threeSum(nums));
 }
--- LeetCode-main/main.cpp
+++ LeetCode-main/main.cpp
@@ -7,13 +7,13 @@
 //
 
 #include <iostream>
-#include "RemoveDuplicatesFromSortedListII.hpp"
+#include "3Sum.hpp"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     
-    testDeleteDuplicatesII();
+    testThreeSum();
     
     return 0;
 }
